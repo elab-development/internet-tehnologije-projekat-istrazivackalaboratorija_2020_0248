4e7508ac52ed58637d7d6401e29b6c1e537e0ba9
nevenajankovicc <nevenajankovic00108@gmail.com>
2024-01-09 21:59:42 +0100

    kreiran kontroler za Comment model

---
+++ laravel/app/Http/Controllers/CommentController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        $comments = Comment::all();
+        return response()->json($comments);
+    }
+
+    public function show($id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+        return response()->json($comment);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'content' => 'required',
+            'user_id' => 'required|exists:users,id',
+            'article_id' => 'required|exists:articles,id',
+            'status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $comment = Comment::create($validator->validated());
+        return response()->json($comment, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'content' => 'sometimes|required',
+            'user_id' => 'sometimes|required|exists:users,id',
+            'article_id' => 'sometimes|required|exists:articles,id',
+            'status' => 'sometimes|required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $comment->update($validator->validated());
+        return response()->json($comment);
+    }
+
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
+        }
+
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted']);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('articles', ArticleController::class);
+
+Route::prefix('comments')->group(function () {
+    Route::get('/', [CommentController::class, 'index']);
+    Route::get('/{id}', [CommentController::class, 'show']);
+    Route::post('/', [CommentController::class, 'store']);
+    Route::put('/{id}', [CommentController::class, 'update']);
+    Route::delete('/{id}', [CommentController::class, 'destroy']);
+});
