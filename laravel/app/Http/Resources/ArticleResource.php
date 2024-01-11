f046d9a806ed15d790c47f7bfd1c1456702044c5
minadjeric <mina.djeric@gmail.com>
2024-01-11 23:49:44 +0100

    kreirani resource-i za modele

---
+++ laravel/app/Http/Resources/ArticleResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'user' => $this->user,
+            'published_at' => $this->published_at,
+            'image_path' => $this->image_path,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
