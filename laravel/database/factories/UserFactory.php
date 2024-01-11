083d867fdeb083d0b300bd65351f52d54abd2d57
nevenajankovicc <nevenajankovic00108@gmail.com>
2024-01-11 23:52:41 +0100

    kreiran factory za korisnika

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -18,10 +18,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'), 
+            'role_id' => 1,  
+            'profile_photo' => 'default.jpg',  
+            'bio' => $this->faker->paragraph,
+            'last_login_at' => $this->faker->dateTimeThisMonth,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
