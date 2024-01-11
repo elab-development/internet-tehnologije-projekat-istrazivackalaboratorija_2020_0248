18f86161c63524cff897690feebcdf9bd413301b
nevenajankovicc <nevenajankovic00108@gmail.com>
2024-01-12 00:06:46 +0100

    kreirani seederi na bekendu

--- laravel/database/seeders/TagSeeder.php
+++ laravel/database/seeders/TagSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TagSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = [
+            ['name' => 'Biologija'],
+            ['name' => 'Hemija'],
+            ['name' => 'Fizika'],
+            ['name' => 'Medicina'],
+            ['name' => 'Informatika'],
+        ];
+
+        DB::table('tags')->insert($tags);
     }
 }
