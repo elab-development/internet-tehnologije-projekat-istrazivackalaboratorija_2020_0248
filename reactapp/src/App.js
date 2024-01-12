43f6c3901b62012ee5cb4df4fcd29fd9aa0bef40
minadjeric <mina.djeric@gmail.com>
2024-01-12 01:05:36 +0100

    dodate rute

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import HomePage from './HomePage';
 import PublicationUploadForm from './PublicationUploadForm ';
 import PublicationsList from './PublicationList';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
-      <PublicationUploadForm></PublicationUploadForm>
-      <PublicationsList></PublicationsList>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<PublicationUploadForm />} />
+          <Route path="/publications" element={<PublicationsList />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
